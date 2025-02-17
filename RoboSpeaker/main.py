5061dcf1e713e6e57dd294bb079e29311751c0d5
AnuradhaSahoo12 <146232685+AnuradhaSahoo12@users.noreply.github.com>
2025-02-17 10:56:05 +0530

    Update main.py

--- RoboSpeaker/main.py
+++ RoboSpeaker/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     while True:
         x = input("Enter what you want me to speak (or type 'q' to quit): ")
         if x.lower() == "q":
-            command = 'powershell -Command "Add-Type -AssemblyName System.speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'Bye bye, rude girl\')"'
+            command = 'powershell -Command "Add-Type -AssemblyName System.speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'Bye bye\')"'
             os.system(command)
             break
         else:
